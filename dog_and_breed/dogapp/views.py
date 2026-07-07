dc236799886279f104ed4567719eab6430988d54
KryostniySergo <calve45@mail.ru>
2026-07-07 21:37:09 +0000

    Добавил вычисления avg_breed_age и same_breed_count в view

--- dog_and_breed/dogapp/views.py
+++ dog_and_breed/dogapp/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Avg, Count, IntegerField, OuterRef, Subquery
+from django.db.models.manager import BaseManager
 from rest_framework import viewsets
 
 from dogapp.serializers import BreedSerializer, DogSerializer
@@ -8,6 +10,24 @@ from .models import Breed, Dog
 class DogViewSet(viewsets.ModelViewSet):
     serializer_class = DogSerializer
     queryset = Dog.objects.all()
+
+    def get_queryset(self) -> BaseManager[Dog]:
+        match self.action:
+            case "list":
+                avg_age_subquery = (
+                    Dog.objects.filter(breed=OuterRef("breed"))
+                    .values("breed")
+                    .annotate(avg_age=Avg("age"))
+                    .values("avg_age")
+                )
+
+                return Dog.objects.select_related("breed").annotate(
+                    avg_breed_age=Subquery(avg_age_subquery, output_field=IntegerField())
+                )
+            case "retrieve":
+                return Dog.objects.select_related("breed").annotate(same_breed_count=Count("breed__dogs"))
+            case _:
+                return Dog.objects.all()
 
 
 class BreedViewSet(viewsets.ModelViewSet):

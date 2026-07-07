c7f6f8086ae190966934363fec64463b704d6be3
KryostniySergo <calve45@mail.ru>
2026-07-07 21:27:53 +0000

    Создал первые view

--- dog_and_breed/dogapp/views.py
+++ dog_and_breed/dogapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from dogapp.serializers import BreedSerializer, DogSerializer
+
+from .models import Breed, Dog
+
+
+class DogViewSet(viewsets.ModelViewSet):
+    serializer_class = DogSerializer
+    queryset = Dog.objects.all()
+
+
+class BreedViewSet(viewsets.ModelViewSet):
+    serializer_class = BreedSerializer
+    queryset = Breed.objects.all()

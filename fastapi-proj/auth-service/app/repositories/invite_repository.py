0c018f8b795e3d473447cd18c3fddc21a11afccb
KryostniySergo <calve45@mail.ru>
2026-08-27 22:46:40 +0000

    Добавил поиск свежего invite по account_id

--- fastapi-proj/auth-service/app/repositories/invite_repository.py
+++ fastapi-proj/auth-service/app/repositories/invite_repository.py
@@ -18,3 +18,20 @@ class InviteRepository(BaseRepository[Invite]):
         """
         result = await self.session.execute(select(Invite).where(Invite.token == token))
         return result.scalar_one_or_none()
+
+    async def get_by_account_id(self, account_id):
+        """Находит самый свежий Invite для данного account_id.
+
+        Args:
+            account_id (UUID): идентификатор аккаунта.
+
+        Returns:
+            Invite | None: найденный инвайт, либо None, если для аккаунта нет инвайтов.
+        """
+        result = await self.session.execute(
+            select(Invite)
+            .where(Invite.account_id == account_id)
+            .order_by(Invite.created_at.desc())
+            .limit(1)  # Нужно чтобы брать самый свежий инвайт
+        )
+        return result.scalar_one_or_none()

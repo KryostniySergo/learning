4c98d775e8775d96eadea28806f83e857a2d3037
KryostniySergo <calve45@mail.ru>
2026-08-30 22:41:24 +0000

    +db_session

---
+++ fastapi-proj/org-service/app/db/session.py
@@ -0,0 +1,11 @@
+from app.core.config import settings
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+engine = create_async_engine(settings.database_url, echo=False, future=True)
+
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
